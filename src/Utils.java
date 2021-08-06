5282f1be0190ab40ea833ce9ce595add8cba8fe5
Robert Overs <oversr14@gmail.com>
2021-08-06 13:35:29 +1000

    Add Utils class

---
+++ src/Utils.java
@@ -0,0 +1,16 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Utils {
+
+    protected BufferedImage loadImage(String imgPath) {
+        try {
+            return ImageIO.read(new File(imgPath));
+        } catch (IOException exc) {
+            System.out.println("Error opening image file: " + exc.getMessage());
+        }
+        return null;
+    }
+}
